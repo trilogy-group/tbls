590781f4204957939b2a540c942c908e53dd6adc
Yauhen Pyl <yauhen.pyl@protonmail.com>
2022-03-24 17:21:43 +0100

    add views and triggers

--- drivers/oracle/oracle.go
+++ drivers/oracle/oracle.go
@@ -52,7 +52,6 @@ func (p *Oracle) Analyze(s *schema.Schema) error {
 		}
 	}
 
-	// select table_name, constraint_name, search_condition text from all_constraints where constraint_type = 'C' and constraint_name not like 'SYS%'
 	tableRows, err := p.db.Query(tablesQuery())
 	if err != nil {
 		return errors.WithStack(err)
@@ -81,7 +80,7 @@ func (p *Oracle) Analyze(s *schema.Schema) error {
 
 		constraints := []*schema.Constraint{}
 
-		relationsRows, err := p.db.Query(queryForRelations(), tableOwner, tableName)
+		relationsRows, err := p.db.Query(queryForRelations, tableOwner, tableName)
 		if err != nil {
 			return errors.WithStack(err)
 		}
@@ -122,7 +121,7 @@ func (p *Oracle) Analyze(s *schema.Schema) error {
 			}
 			constraints = append(constraints, constraint)
 		}
-		constrainsRows, err := p.db.Query(queryForConstraints(), tableOwner, tableName)
+		constrainsRows, err := p.db.Query(queryForConstraints, tableOwner, tableName)
 		if err != nil {
 			return errors.WithStack(err)
 		}
@@ -151,44 +150,13 @@ func (p *Oracle) Analyze(s *schema.Schema) error {
 		}
 		table.Constraints = constraints
 
-		columnRows, err := p.db.Query(columnsQuery(), tableOwner, tableName)
+		columns, err := p.getColumns(tableColumnsQuery, tableOwner, tableName)
 		if err != nil {
 			return errors.WithStack(err)
 		}
-		defer columnRows.Close()
-		columns := []*schema.Column{}
-		for columnRows.Next() {
-			var (
-				columnName    string
-				isNullable    string
-				dataType      string
-				dataDefault   sql.NullString
-				columnComment sql.NullString
-			)
-			err = columnRows.Scan(&columnName, &isNullable, &dataType, &dataDefault, &columnComment)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-			column := &schema.Column{
-				Name:    columnName,
-				Type:    dataType,
-				Default: dataDefault,
-				Comment: columnComment.String,
-			}
-			switch isNullable {
-			case "N":
-				column.Nullable = false
-			case "Y":
-				column.Nullable = true
-			default:
-				return errors.Errorf("unsupported col.nullable value '%s'", isNullable)
-			}
-
-			columns = append(columns, column)
-		}
 		table.Columns = columns
 
-		indexRows, err := p.db.Query(queryForIndexes(), tableOwner, tableName)
+		indexRows, err := p.db.Query(queryForIndexes, tableOwner, tableName)
 		if err != nil {
 			return errors.WithStack(err)
 		}
@@ -218,8 +186,45 @@ func (p *Oracle) Analyze(s *schema.Schema) error {
 		}
 		table.Indexes = indexes
 
+		triggerRows, err := p.db.Query(queryForTriggers, tableOwner, tableName)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		defer triggerRows.Close()
+
+		triggers := []*schema.Trigger{}
+		for triggerRows.Next() {
+			var (
+				triggerName string
+				triggerDef  string
+				//triggerComment sql.NullString
+			)
+			err = triggerRows.Scan(&triggerName, &triggerDef)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+			trigger := &schema.Trigger{
+				Name: triggerName,
+				Def:  triggerDef,
+			}
+			triggers = append(triggers, trigger)
+		}
+		table.Triggers = triggers
+
 		tables = append(tables, table)
 	}
+
+	views, err := p.getViews(viewsQuery())
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	tables = append(tables, views...)
+
+	materializedViews, err := p.getViews(materializedViewsQuery())
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	tables = append(tables, materializedViews...)
 
 	s.Tables = tables
 	relations := []*schema.Relation{}
@@ -258,6 +263,83 @@ func (p *Oracle) Analyze(s *schema.Schema) error {
 	return nil
 }
 
+func (p *Oracle) getViews(query string) ([]*schema.Table, error) {
+	viewRows, err := p.db.Query(query)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	defer viewRows.Close()
+
+	tables := []*schema.Table{}
+	for viewRows.Next() {
+		var (
+			viewName       string
+			viewType       string
+			viewOwner      string
+			viewComment    sql.NullString
+			viewDefinition sql.NullString
+		)
+		err := viewRows.Scan(&viewName, &viewType, &viewOwner, &viewComment, &viewDefinition)
+		if err != nil {
+			return tables, errors.WithStack(err)
+		}
+		table := &schema.Table{
+			Name:    fullTableName(viewOwner, viewName),
+			Type:    viewType,
+			Comment: viewComment.String,
+			Def:     viewDefinition.String,
+		}
+
+		columns, err := p.getColumns(viewColumnsQuery, viewOwner, viewName)
+		if err != nil {
+			return tables, errors.WithStack(err)
+		}
+		table.Columns = columns
+
+		tables = append(tables, table)
+	}
+	return tables, nil
+}
+
+func (p *Oracle) getColumns(query string, owner string, name string) ([]*schema.Column, error) {
+	columnRows, err := p.db.Query(query, owner, name)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	defer columnRows.Close()
+	columns := []*schema.Column{}
+	for columnRows.Next() {
+		var (
+			columnName    string
+			isNullable    string
+			dataType      string
+			dataDefault   sql.NullString
+			columnComment sql.NullString
+		)
+		err = columnRows.Scan(&columnName, &isNullable, &dataType, &dataDefault, &columnComment)
+		if err != nil {
+			return columns, errors.WithStack(err)
+		}
+		column := &schema.Column{
+			Name:    columnName,
+			Type:    dataType,
+			Default: dataDefault,
+			Comment: columnComment.String,
+		}
+		switch isNullable {
+		case "N":
+			column.Nullable = false
+		case "Y":
+			column.Nullable = true
+		default:
+			return columns, errors.Errorf("unsupported col.nullable value '%s'", isNullable)
+		}
+
+		columns = append(columns, column)
+	}
+	return columns, nil
+}
+
 func constraintTypeFullName(constraintType string) string {
 	switch constraintType {
 	case "R":
@@ -291,6 +373,8 @@ func (p *Oracle) Info() (*schema.Driver, error) {
 	return d, nil
 }
 
+// queries came from https://dataedo.com/kb/query/oracle
+
 var filteredOwners = `not in ('ANONYMOUS','CTXSYS','DBSNMP','EXFSYS', 'LBACSYS',
 'MDSYS', 'MGMT_VIEW','OLAPSYS','OWBSYS','ORDPLUGINS', 'ORDSYS','OUTLN',
 'SI_INFORMTN_SCHEMA','SYS','SYSMAN','SYSTEM', 'TSMSYS','WK_TEST',
@@ -308,8 +392,7 @@ JOIN all_tab_comments atc ON atc.OWNER = ata.OWNER AND ata.TABLE_NAME = atc.TABL
 WHERE ata.OWNER %s`, filteredOwners)
 }
 
-func columnsQuery() string {
-	return `select col.column_name, 
+var tableColumnsQuery = `select col.column_name, 
 			col.nullable,
 			col.data_type, 
 			col.DATA_DEFAULT,
@@ -318,19 +401,53 @@ func columnsQuery() string {
 	inner join sys.all_tables t on col.owner = t.owner and col.table_name = t.table_name
 	INNER JOIN all_tab_comments atc ON atc.OWNER = col.OWNER AND col.TABLE_NAME = atc.TABLE_NAME 
 	where col.owner = :tableOwner and col.table_name = :tableName`
+
+func viewsQuery() string {
+	return fmt.Sprintf(`SELECT
+  av.view_name, atc.TABLE_TYPE, av.OWNER, atc.COMMENTS, av.text_vc AS description
+FROM
+  all_views av
+JOIN all_tab_comments atc ON atc.OWNER = av.OWNER AND av.view_name = atc.TABLE_NAME 
+WHERE av.OWNER %s`, filteredOwners)
 }
 
-func queryForRelations() string {
-	return `SELECT distinct c.CONSTRAINT_NAME, c.CONSTRAINT_TYPE, a.column_name child_column, 
+var viewColumnsQuery = `select col.column_name, 
+			col.nullable,
+			col.data_type, 
+			col.DATA_DEFAULT,
+			atc.COMMENTS
+	from sys.all_tab_columns col
+	inner join sys.all_views t on col.owner = t.owner and col.table_name = t.view_name
+	INNER JOIN all_tab_comments atc ON atc.OWNER = col.OWNER AND col.TABLE_NAME = atc.TABLE_NAME 
+	where col.owner = :viewOwner and col.table_name = :viewName`
+
+func materializedViewsQuery() string {
+	return fmt.Sprintf(`SELECT
+	amv.mview_name, atc.TABLE_TYPE, amv.OWNER, atc.COMMENTS, query as definition
+FROM
+	all_mviews amv
+JOIN all_tab_comments atc ON atc.OWNER = amv.OWNER AND amv.mview_name = atc.TABLE_NAME 
+WHERE amv.OWNER %s`, filteredOwners)
+}
+
+var materializedViewColumnsQuery = `select col.column_name, 
+			col.nullable,
+			col.data_type, 
+			col.DATA_DEFAULT,
+			atc.COMMENTS
+	from sys.all_tab_columns col
+	inner join sys.all_mviews t on col.owner = t.owner and col.table_name = t.mview_name
+	INNER JOIN all_tab_comments atc ON atc.OWNER = col.OWNER AND col.TABLE_NAME = atc.TABLE_NAME 
+	where col.owner = :viewOwner and col.table_name = :viewName`
+
+var queryForRelations = `SELECT distinct c.CONSTRAINT_NAME, c.CONSTRAINT_TYPE, a.column_name child_column, 
 	b.table_name parent_table, b.column_name parent_column, b.OWNER as parent_table_owner
 FROM all_constraints c
 JOIN all_cons_columns a ON a.owner = c.owner AND a.constraint_name = c.constraint_name
 join all_cons_columns b on c.owner = b.owner and c.r_constraint_name = b.constraint_name
 WHERE c.CONSTRAINT_TYPE = 'R' and a.owner = :tableOwner and a.table_name = :tableName`
-}
 
-func queryForConstraints() string {
-	return `select ctr.constraint_name, 
+var queryForConstraints = `select ctr.constraint_name, 
 	ctr.CONSTRAINT_TYPE,
 	col.column_name,
 	ctr.SEARCH_CONDITION as constraint,
@@ -338,14 +455,21 @@ func queryForConstraints() string {
 from sys.all_constraints ctr
 join sys.all_cons_columns col on ctr.owner = col.owner and ctr.constraint_name = col.constraint_name and ctr.table_name = col.table_name
 WHERE ctr.CONSTRAINT_TYPE = 'C' and ctr.owner = :tableOwner and ctr.table_name = :tableName`
-}
 
-func queryForIndexes() string {
-	return `select ind.index_name,
+var queryForIndexes = `select ind.index_name,
 	ind_col.column_name,
 	ind.index_type
 from sys.all_indexes ind
 inner join sys.all_ind_columns ind_col on ind.owner = ind_col.index_owner
 	and ind.index_name = ind_col.index_name
 WHERE ind.table_owner = :tableOwner and ind.table_name = :tableName`
-}
+
+/*
+	trig.trigger_type,
+	trig.triggering_event,
+	trig.status,
+*/
+var queryForTriggers = `select trig.trigger_name,
+	trig.trigger_body as script 
+from sys.all_triggers trig
+WHERE trig.table_owner = :tableOwner and trig.table_name = :tableName`
